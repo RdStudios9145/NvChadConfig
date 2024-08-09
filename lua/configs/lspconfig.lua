174f39bee86ffe3f33b6811082f36ac3aaf08470
Holden Seidel <business@rdstudios.org>
2024-08-08 18:19:43 -0700

    little cleaning, finally fixed ag command

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,26 +1,19 @@
--- EXAMPLE 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+-- EXAMPLE
+local on_attach = require('nvchad.configs.lspconfig').on_attach
+local on_init = require('nvchad.configs.lspconfig').on_init
+local capabilities = require('nvchad.configs.lspconfig').capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "zls" }
+local lspconfig = require('lspconfig')
+local servers = { 'html', 'cssls', 'zls', 'tsserver' }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig[lsp].setup({
     on_attach = on_attach,
     on_init = on_init,
     capabilities = capabilities,
-  }
+  })
 end
-
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
 
 local M = {
   on_attach = on_attach,
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,132 +1,119 @@
-local cmp = require("cmp")
+local cmp = require('cmp')
 
 return {
   {
-    "stevearc/conform.nvim",
+    'stevearc/conform.nvim',
     event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
+    lazy = false,
+    config = function() require('configs.conform') end,
+  },
+  {
+    'rust-lang/rust.vim',
+    ft = 'rust',
+    init = function()
+      -- vim.g.rustfmt_autosave = 1
     end,
   },
   {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function ()
-      vim.g.rustfmt_autosave = 1
-    end
-  },
-  {
-    "mileszs/ack.vim",
-    init = function ()
-      vim.g.ackpkg = "ag --nogroup --column"
-    end
-  },
-  {
-    "junegunn/fzf.vim",
+    'junegunn/fzf',
     lazy = false,
   },
   {
-    "williamboman/mason.nvim",
+    'junegunn/fzf.vim',
+    lazy = false,
+  },
+  {
+    'williamboman/mason.nvim',
     opts = {
       ensure_installed = {
-        "rust-analyzer",
-        "zls"
+        'rust-analyzer',
+        'zls',
       },
     },
   },
   {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
+    'neovim/nvim-lspconfig',
+    config = function() require('configs.lspconfig') end,
   },
   {
-    "mrcjkb/rustaceanvim",
-    version = "^4",
-    ft = { "rust" },
-    dependencies = "neovim/nvim-lspconfig",
-    config = function ()
-      require "configs.rustaceanvim"
-    end
+    'mrcjkb/rustaceanvim',
+    version = '^4',
+    ft = { 'rust' },
+    dependencies = 'neovim/nvim-lspconfig',
+    config = function() require('configs.rustaceanvim') end,
   },
   {
-    "mfussenegger/nvim-dap",
+    'mfussenegger/nvim-dap',
   },
   {
     'saecki/crates.nvim',
-    ft = { "toml" },
+    ft = { 'toml' },
     config = function(_, opts)
-      local crates  = require('crates')
+      local crates = require('crates')
       crates.setup(opts)
 
       require('cmp').setup.buffer({
-        sources = { { name = "crates" }}
+        sources = { { name = 'crates' } },
       })
 
       crates.show()
       -- require("core.utils").load_mappings("crates")
-      vim.keymap.set("n", "<leader>rcu", function() require("crates").upgrade_all_crates() end)
+      vim.keymap.set('n', '<leader>rcu', function() require('crates').upgrade_all_crates() end)
     end,
   },
   {
-    "theHamsta/nvim-dap-virtual-text",
+    'theHamsta/nvim-dap-virtual-text',
     lazy = false,
-    config = function(_, _)
-      require("nvim-dap-virtual-text").setup()
-    end
+    config = function(_, _) require('nvim-dap-virtual-text').setup() end,
   },
   {
-    "hrsh7th/nvim-cmp",
+    'hrsh7th/nvim-cmp',
     opts = function()
-      local cmp = require "cmp"
+      local cmp = require('cmp')
 
       cmp.setup({
         snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
+          expand = function(args) vim.snippet.expand(args.body) end,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
         }),
         sources = cmp.config.sources({
-          { name = "crates" },
+          { name = 'crates' },
         }),
       })
       return M
     end,
   },
   {
-    "mg979/vim-visual-multi",
-    branch = "master",
+    'mg979/vim-visual-multi',
+    branch = 'master',
     lazy = false,
     init = function()
       vim.g.VM_maps = {
-        ["Add Cursor Down"] = '<M-j>',
-        ["Add Cursor Up"]   = '<M-k>',
+        ['Add Cursor Down'] = '<M-j>',
+        ['Add Cursor Up'] = '<M-k>',
       }
     end,
   },
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function() vim.fn['mkdp#util#install']() end,
+  },
+  {
+    'Shatur/neovim-session-manager',
+    lazy = false,
+    init = function()
+      local ses = require('session_manager')
+      ses.setup({
+        autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
+      })
     end,
   },
   {
-    "Shatur/neovim-session-manager",
-    lazy = false,
-    init = function()
-      local ses = require("session_manager")
-      ses.setup({
-        autoload_mode = require("session_manager.config").AutoloadMode.CurrentDir,
-      })
-    end
+    'ziglang/zig.vim',
+    ft = { 'zig' },
   },
-  {
-    "ziglang/zig.vim",
-    ft = { "zig" },
-  }
 }
